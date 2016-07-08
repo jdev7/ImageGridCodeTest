7386edf6d1ccb18a0aeabed359fffef57d790cb8
Juan Navas <juannavas7@gmail.com>
2016-07-08 18:37:58 +0200

    Initial Commit

---
+++ ImageGridCodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGridCodeTest
+//
+//  Created by Juan Navas Martin on 08/07/16.
+//  Copyright © 2016 Perhapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageGridCodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageGridCodeTest
+//
+//  Created by Juan Navas Martin on 08/07/16.
+//  Copyright © 2016 Perhapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
